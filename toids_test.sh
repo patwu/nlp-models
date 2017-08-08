d3a035a9cec16da8f6dcaeed3402f36e5e0ecf66
tst <329126089@qq.com>
2017-08-08 14:17:03 +0800

    gcc版本导致库文件编译问题

--- toids_test.sh
+++ toids_test.sh
@@ -1,7 +1,7 @@
 rm -f toids_op.so
 
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-
-g++ -I. -L. -std=c++11 -shared toids_op.cc -o toids_op.so -fPIC -I $TF_INC -O2  
-
+echo $TF_INC
+#g++ -I. -L. -std=c++11 -shared toids_op.cc -o toids_op.so -fPIC -I $TF_INC -O2
+g++ -I. -L. -std=c++11 -shared toids_op.cc -o toids_op.so -fPIC -I $TF_INC -O2 -D_GLIBCXX_USE_CXX11_ABI=0 
 python toids_op_test.py
